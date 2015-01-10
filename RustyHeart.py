124ed78f9faee3e31094f0b0ea00b4e138daebe8
Beatrice Liang <beatricesliang@gmail.com>
2015-01-09 21:47:11 -0500

    Edited RustyHeart.py

--- RustyHeart.py
+++ RustyHeart.py
@@ -9,6 +9,7 @@ class RustyHeart:
 		self.state = "start"
 		pygame.init()
 
+		self.screensize = (640,480)
 		#set up fonts
 		try:
 			pygame.font.init()
@@ -18,33 +19,48 @@ class RustyHeart:
 		#set up clock
 		self.clock = pygame.time.Clock()
 		#create screen
-		self.screen = pygame.display.set_mode((640,480))
+		self.screen = pygame.display.set_mode(self.screensize)
 		
-	def drawBkg(screen, refresh, text = None, rect = None):
-		if rect == None:
-			self.screen.fill((255,255,255))
-			if text != None:
-				self.screen.blit(text,(10,10))
-			refresh.append(screen.get_rect)
-		else:
-			screen.fill( (255, 255, 255), rect )
+	def drawBkg(self,screen, refresh, text = None, imageName = None, rect = None):
+			if imageName != None:
+				background = pygame.image.load(imageName).convert_alpha()
+			if rect == None:
+				if imageName == None:
+					self.screen.fill((255,255,255))
+				else:
+					self.screen.blit(background,(-30,-30))
+				if text != None:
+					self.screen.blit(text,(10,10))
+				refresh.append(screen.get_rect())
+			else:
+				if imageName == None:
+					self.screen.fill( (255, 255, 255), rect )
+				else:
+					self.screen.blit(background(-30,-30))
 
-			# blit the text surface onto the screen if it is inside the rectangle
-			screen.fill( (255, 255, 255), text.get_rect().move(10, 10).clip( rect ) )
+				# blit the text surface onto the screen if it is inside the rectangle
+				if text != None:
+					self.screen.fill( (255, 255, 255), text.get_rect().move(10, 10).clip( rect ) )
 
-			trect = text.get_rect().move(10, 10) # rectangle in which to
-											 # draw the text
-											 
-			clippedRect = trect.clip( rect ) # intersection of the text
-										 # screen rectangle and the
-										 # area to update
+					trect = text.get_rect().move(10, 10) # rectangle in which to
+												 # draw the text						 
+					clippedRect = trect.clip( rect ) # intersection of the text
+											 # screen rectangle and the
+											 # area to update
+					# blit the text into the area to update, the second rectangle
+					# indicates which part of the text to use
+					urect = screen.blit( text, clippedRect, clippedRect.move(-10,-10) )
 
-			# blit the text into the area to update, the second rectangle
-			# indicates which part of the text to use
-			urect = screen.blit( text, clippedRect, clippedRect.move(-10,-10) )
+				# refresh the rectangle
+				refresh.append( rect )
 
-			# refresh the rectangle
-			refresh.append( rect )
+
+
+
+			
+
+
+
 
 
 
